58d4feafc0c4322c0b2597be7e4f54696936abcc
Antonio Furtado <antoniordf@hotmail.com>
2023-12-27 16:41:55 +0000

    Code refactor

--- src/instruction.rs
+++ src/instruction.rs
@@ -7,11 +7,6 @@ pub enum MovieInstruction {
         rating: u8,
         description: String,
     },
-    UpdateMovieReview {
-        title: String,
-        rating: u8,
-        description: String,
-    },
 }
 
 #[derive(BorshDeserialize)]
@@ -22,24 +17,13 @@ struct MovieReviewPayload {
 }
 
 impl MovieInstruction {
-    // Unpack inbound buffer to associated Instruction
-    // The expected format for input is a Borsh serialized vector
     pub fn unpack(input: &[u8]) -> Result<Self, ProgramError> {
-        // Split the first byte of data
         let (&variant, rest) = input
             .split_first()
             .ok_or(ProgramError::InvalidInstructionData)?;
-        // `try_from_slice` is one of the implementations from the BorshDeserialization trait
-        // Deserializes instruction byte data into the payload struct
         let payload = MovieReviewPayload::try_from_slice(rest).unwrap();
-        // Match the first byte and return the AddMovieReview struct
         Ok(match variant {
             0 => Self::AddMovieReview {
-                title: payload.title,
-                rating: payload.rating,
-                description: payload.description,
-            },
-            1 => Self::UpdateMovieReview {
                 title: payload.title,
                 rating: payload.rating,
                 description: payload.description,
